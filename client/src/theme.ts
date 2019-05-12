01606f3eb1146ea9b2e9930c1c7713bbc7459429
Ragnar-H <ragnarhardarson90@gmail.com>
2019-05-12 13:16:05 +0200

    Add clowny theme

---
+++ client/src/theme.ts
@@ -0,0 +1,10 @@
+export const primaryColor = '#ff4081'
+export const primaryLightColor = '#ff79b0'
+export const primaryDarkColor = '#c60055'
+export const secondaryColor = '#673ab7'
+export const secondaryLightColor = '#9a67ea'
+export const secondaryDarkColor = '#320b86'
+export const primaryTextColor = '#3f51b5'
+export const secondaryTextColor = '#ffb300'
+
+export const sizingUnit = 8
