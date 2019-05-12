881e02bd72a5e0834566514b1a345a40cbec9e06
Ragnar-H <ragnarhardarson90@gmail.com>
2019-05-12 12:05:27 +0200

    Calculate arrow head from line coords
    Keeping everything nice and uniform

--- client/src/Command.tsx
+++ client/src/Command.tsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import { CommandAction } from 'DraggableCommands'
 
-type DroneCommand =
-  | {
-      action: CommandAction
-      speed: number
-      distance: number
-    }
-  | { action: 'command' | 'streamon' | 'streamoff' | 'land' }
+export const MAX_SPEED = 100
+
+type DroneCommand = {
+  action: CommandAction
+  speed: number
+  distance: number
+}
 
 type Props = {
   command: DroneCommand
 }
 
+function getInitialCoordinates(command: DroneCommand) {
+  const minArrowLength = 8
+  const speed = (command.speed / MAX_SPEED) * 8 + minArrowLength
+
+  const tip = { x: 4, y: 16 }
+  const base = { x: 48, y: 16 }
+  const head = {
+    x1: tip.x + speed,
+    y1: tip.y - speed,
+    x2: tip.x,
+    y2: tip.y,
+    x3: tip.x + speed,
+    y3: tip.y + speed,
+  }
+
+  return { tip, base, head }
+}
+
 export function Command(props: Props) {
+  const { tip, base, head } = getInitialCoordinates(props.command)
   return (
     <svg
       width="48"
@@ -22,8 +41,12 @@ export function Command(props: Props) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M4 16L48 16" stroke="red" strokeWidth="4" />
-      <path d="M16 2L4 16L16 30" stroke="black" strokeWidth="4" />
+      <path d={`M${tip.x} ${tip.y}L${base.x} ${base.y}`} stroke="red" strokeWidth="4" />
+      <path
+        d={`M${head.x1} ${head.y1}L${head.x2} ${head.y2}L${head.x3} ${head.y3}`}
+        stroke="black"
+        strokeWidth="4"
+      />
     </svg>
   )
 }
