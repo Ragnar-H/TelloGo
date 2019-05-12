8103bdcdee8cbf430b9ed78b27a97ed27cc8e580
Ragnar-H <ragnarhardarson90@gmail.com>
2019-05-12 09:46:18 +0200

    Add center decorator

---
+++ client/.storybook/decorators.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function center(storyFn) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+      }}
+    >
+      {storyFn()}
+    </div>
+  )
+}
