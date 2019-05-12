42c524a8e7f0496613ccc1e6780c5676d90701ad
Ragnar-H <ragnarhardarson90@gmail.com>
2019-05-12 12:21:14 +0200

    Add knobs to Command

--- client/stories/Command.stories.tsx
+++ client/stories/Command.stories.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-
+import { withKnobs, number } from '@storybook/addon-knobs'
 import { Command } from '../src/Command'
 import { center } from '../.storybook/decorators'
 
 storiesOf('Command', module)
+  .addDecorator(withKnobs)
   .addDecorator(center)
-  .add('default', () => <Command command={{ action: 'left', speed: 10, distance: 20 }} />)
+  .add('default', () => (
+    <Command
+      command={{
+        action: 'left',
+        speed: number('Speed', 10, { min: 10, max: 100, range: true, step: 1 }),
+        distance: number('Distance', 20, { min: 20, max: 500, range: true, step: 1 }),
+      }}
+    />
+  ))
   .add('multiple', () => (
     <div>
       <Command command={{ action: 'left', speed: 10, distance: 20 }} />
