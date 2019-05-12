3dd495f86fa7ea6034ca224ab007466b8be8ce15
Ragnar-H <ragnarhardarson90@gmail.com>
2019-05-12 15:00:24 +0200

    Put some colors on Speed and Distance input

--- client/src/Command.tsx
+++ client/src/Command.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { CommandAction } from 'DraggableCommands'
 import { useSpring, animated } from 'react-spring'
-import { secondaryTextColor } from './theme'
+import {
+  primaryTextColor,
+  secondaryTextColor,
+  secondaryDarkColor,
+  sizingUnit,
+} from './theme'
 export const MAX_SPEED = 100
 export const MAX_DISTANCE = 500
 type DroneCommand = {
@@ -59,24 +64,76 @@ export function Command(props: Props) {
     arrowPath,
   })
   return (
-    <svg
-      width="100"
-      height="100"
-      viewBox="0 0 48 48"
-      fill="none"
-      transform={`rotate(${rotation})`}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <animated.path
-        d={animatedPaths.bodyPath}
-        stroke={secondaryTextColor}
-        strokeWidth="4"
-      />
-      <animated.path
-        d={animatedPaths.arrowPath}
-        stroke={secondaryTextColor}
-        strokeWidth="4"
-      />
-    </svg>
+    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      <svg
+        width="100"
+        height="100"
+        viewBox="0 0 48 48"
+        fill="none"
+        transform={`rotate(${rotation})`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <animated.path
+          d={animatedPaths.bodyPath}
+          stroke={secondaryTextColor}
+          strokeWidth="4"
+        />
+        <animated.path
+          d={animatedPaths.arrowPath}
+          stroke={secondaryTextColor}
+          strokeWidth="4"
+        />
+      </svg>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <label
+          htmlFor="distance"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            color: primaryTextColor,
+          }}
+        >
+          Distance
+        </label>
+        <input
+          id="distance"
+          type="number"
+          value={props.command.distance}
+          style={{
+            minWidth: 0,
+            display: 'flex',
+            color: primaryTextColor,
+            backgroundColor: secondaryDarkColor,
+            border: 0,
+            borderRadius: `${sizingUnit}px`,
+            padding: `${sizingUnit}px`,
+          }}
+        />
+        <label
+          htmlFor="speed"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            color: primaryTextColor,
+          }}
+        >
+          Speed
+        </label>
+        <input
+          id="speed"
+          type="number"
+          value={props.command.speed}
+          style={{
+            minWidth: 0,
+            display: 'flex',
+            color: primaryTextColor,
+            backgroundColor: secondaryDarkColor,
+            border: 0,
+            borderRadius: `${sizingUnit}px`,
+            padding: `${sizingUnit}px`,
+          }}
+        />
+      </div>
+    </div>
   )
 }
--- client/src/theme.ts
+++ client/src/theme.ts
@@ -4,7 +4,7 @@ export const primaryDarkColor = '#c60055'
 export const secondaryColor = '#673ab7'
 export const secondaryLightColor = '#9a67ea'
 export const secondaryDarkColor = '#320b86'
-export const primaryTextColor = '#3f51b5'
+export const primaryTextColor = '#ffe082'
 export const secondaryTextColor = '#ffb300'
 
 export const sizingUnit = 8
